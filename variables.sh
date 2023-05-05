e36babf74915f7dfc072fd277c436fa0fb2ce9d9
Amir Mansoor <69566729+amir-mansoor@users.noreply.github.com>
2023-05-05 17:46:58 +0300

    variables

--- variables.sh
+++ variables.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 echo "Variables"
 echo "Container which will hold some values"
+echo "User defined variables"
 name="mansoor"
 age=18
 echo $name,$age
+
+## NOT GOOD
+cp /my/location/from /my/location/to
+cp /my/location/from/here /my/location/to/there
+
+## BETTER
+MY_LOCATION_FROM=/my/location/from
+MY_LOCATION_TO=/my/location/to
+
+cp $MY_LOCATION_FROM $MY_LOCATION_TO
